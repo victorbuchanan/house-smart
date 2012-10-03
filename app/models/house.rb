c20389e35dfbf3493447a2b2e432c8102931b438
Khaled Alissa <khaled.s.alissa@gmail.com>
2012-10-03 15:07:23 -0400

    house is form of property. initialization and validation

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,5 @@
 class House < ActiveRecord::Base
   # attr_accessible :title, :body
+
+   has_one :property, :as => :propertible, :dependent => :destroy 
 end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe House do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "associations" do
+    it "should extend Property" do  #polymorphism
+      House.new.should respond_to(:property)
+    end
+
+    it "should destroy the associated property class after destroying itself" do
+      house= FactoryGirl.build(:house)
+      house.save
+      House.all.size.should == 1
+      Property.all.size.should == 1
+      House.first.destroy
+      Property.all.size.should == 0
+    end
+
+  end
 end
