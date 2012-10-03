b84b1f4dff7e14d0bf81bf58764c0cb7a9585341
Khaled Alissa <khaled.s.alissa@gmail.com>
2012-10-03 14:58:55 -0400

    name, email presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password_digest
+
+    validates_presence_of :name, :email
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Validations" do
+
+
+    [:name, :email].each do |attr|
+      it "#{attr} cannot be nil or blank" do
+        attr = attr.to_sym
+        anonymous = FactoryGirl.build(:user, attr => "")
+        anonymous.should_not be_valid
+        anonymous.errors.get(attr).should_not be_nil
+        anonymous[attr] = "Luigi"
+        anonymous.should be_valid
+
+      end
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+  end
 end
