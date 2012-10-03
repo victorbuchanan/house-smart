bfa76c1969b61a919f307302304262d370f634bf
Khaled Alissa <khaled.s.alissa@gmail.com>
2012-10-03 15:03:28 -0400

    Property polymorphism. initizalization and validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,6 @@
 class Property < ActiveRecord::Base
   attr_accessible :lot_size, :propertible_id, :propertible_type
+
+    belongs_to :propertible, polymorphic: true
+
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe Property do
   pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @valid_property_attrs = FactoryGirl.attributes_for :property
+  end
+
+  context "associations" do
+
+    it "is a polymorphic object" do
+        house = House.create( property_attributes: @valid_property_attrs )
+        house.property.should be_kind_of Property
+        Property.all.size.should == 1
+        Property.first.propertible_type.should == "House"
+        Property.first.propertible_id.should == 1    #
+    end
+  end
 end
