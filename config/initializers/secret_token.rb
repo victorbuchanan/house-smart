d2ff77eb1aa444f5e2d9136ec1772230526894da
Khaled Alissa <khaled.s.alissa@gmail.com>
2012-10-03 14:39:01 -0400

    app initializaion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseSmart::Application.config.secret_token = '25761bc7a61cbe2347cf8d00d0a72529c9061b4a10a62818b5d48ef14ee68a2898b47b5d5890737d88944de877a2676a8c0719b708849e2aa97b6bc63743471f'
