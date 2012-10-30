d88da8e27f0132b3d27c29bc78bb66556d8657fd
Khaled Alissa <khaled.s.alissa@gmail.com>
2012-10-30 13:29:07 -0400

    navigatibility improvement

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ HouseSmart::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'properties#index'
 
   # See how all your routes lay out with "rake routes"
 
