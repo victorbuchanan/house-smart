7779e8fe04c1eb2244679266b06fd81d433db54f
Samer Al-buhaisi <samer.albuhaisi@gmail.com>
2012-11-17 21:11:43 -0500

    Fixed bug in AreaBasePrice table test

--- spec/models/area_base_price_spec.rb
+++ spec/models/area_base_price_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe AreaBasePrice do
   context "validations" do
-    [:name, :value].each do |attr|
+    [:area_name, :avg_bath, :avg_bed, :avg_lot, :avg_sqft, :avg_year, :price_per_bath, :price_per_bed, :price_per_lot, :price_per_sqft, :price_per_year].each do |attr|
       it "#{attr} may be accessed and changed" do
         expect do
           AreaBasePrice.new(attr => nil)
